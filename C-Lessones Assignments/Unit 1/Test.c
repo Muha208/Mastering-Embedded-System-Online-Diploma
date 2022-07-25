4a12bf9910f25b9fb917364aca74e2ba402c0a55
Muah208 <Muhammad_osama208@hotmail.com>
2022-07-25 06:21:41 +0200

    Test1

--- C-Lessones Assignments/Unit 1/Test.c
+++ C-Lessones Assignments/Unit 1/Test.c
@@ -1,14 +1,12 @@
 /*
- * Assignment 1.c
+ * Test.c
  *
  *  Created on: 20 Jul 2022
  *      Author: Muhammad Osama
  */
+#include<stdio.h>
 
+int main(){
+	printf("Hello Muhammad");
 
-#include <stdio.h>
-
-int main()
-{
-	printf("Hello \n");
 }
