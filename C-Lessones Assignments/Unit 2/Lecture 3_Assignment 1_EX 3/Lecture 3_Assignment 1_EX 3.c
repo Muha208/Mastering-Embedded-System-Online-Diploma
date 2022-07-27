3fb994f6ca01c23c8f2225ee4137f77e3959865a
Muha208 <Muhammad_osama208@hotmail.com>
2022-07-27 14:46:05 +0200

    Lecture 3 Assignment 1 ex 3

--- C-Lessones Assignments/Unit 2/Lecture 3_Assignment 1_EX 3/Lecture 3_Assignment 1_EX 3.c
+++ C-Lessones Assignments/Unit 2/Lecture 3_Assignment 1_EX 3/Lecture 3_Assignment 1_EX 3.c
@@ -5,4 +5,25 @@
  *      Author: Muhammad Osama
  */
 
+/*
+ EX 3
+ Write C Program to Add Two Integers
 
+i should see the Console as following:
+##########Console-output###
+Enter two integers: 12
+11
+Sum: 23
+*/
+
+#include "stdio.h"
+
+int main(void){
+	int x,y;
+	printf("Enter two integers: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%d\n%d",&x, &y);
+	printf("Sum: %d",x+y);
+	fflush(stdin); fflush(stdout);
+	return 0;
+}
